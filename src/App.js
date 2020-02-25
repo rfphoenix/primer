6cd989d6eebfb7b5a9fe7b1601587d24648353bf
Johnson Wang <freelance@rfphoenix.com>
2020-02-25 15:25:33 -0800

    added some function and expression to App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React, { Component } from "react";
 const message = "This is a constant";
 const count = 4;
 
+function isEven() {
+    return count % 2 === 0 ? "Even" : "Odd";
+}
+
 export default class App extends Component {
 
   render = () =>
     <h4 className="bg-primary text-white text-center p-2 m-1">
-        Number of things: { count }
+        Number of things: { isEven() }
     </h4>
+
 }
