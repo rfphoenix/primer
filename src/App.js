d9ecd605a0f2e4f0e37e189d879c941af783db61
Johnson Wang <freelance@rfphoenix.com>
2020-02-25 17:11:59 -0800

    control element class name based on the dynamic value of count

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
 
-const message = "This is a constant";
-const count = 4;
-
-function isEven() {
-    return count % 2 === 0 ? "Even" : "Odd";
-}
-
 export default class App extends Component {
 
-  render = () =>
-    <h4 className="bg-primary text-white text-center p-2 m-1">
-        Number of things: { isEven() }
-    </h4>
+  constructor(props) {
+      super(props);
+      this.state = {
+        count : 4
+      }
+  }
 
+  isEven(val) {
+    return val % 2 === 0 ? "Even" : "Odd";
+  }
+
+  getClassName(val) {
+    return val % 2 === 0
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secondary text-white text-center p-2 m-1"
+  }
+
+  handleClick = () => this.setState({ count: this.state.count + 1});
+ 
+  render = () =>
+    <h4 className={ this.getClassName( this.state.count ) } >
+      <button className="btn btn-info m-2" onClick={ this.handleClick }>Click Me</button>
+        Number of things: { this.isEven( this.state.count ) }
+    </h4>
 }
